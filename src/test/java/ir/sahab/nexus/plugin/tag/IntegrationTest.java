b7c80354890e0482c821fb491f295fc8a0e66aa9
Ali Reza Talebipour <talebipour@inside.sahab.ir>
2020-06-11 16:13:56 +0430

    Improve integration test

--- src/test/java/ir/sahab/nexus/plugin/tag/IntegrationTest.java
+++ src/test/java/ir/sahab/nexus/plugin/tag/IntegrationTest.java
@@ -4,6 +4,7 @@ import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonList;
 import static java.util.Collections.singletonMap;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -66,9 +67,9 @@ public class IntegrationTest {
     public void setup() {
         client = ClientBuilder.newClient();
         target = client.target("http://nexus:8081/service/rest" + APIConstants.V1_API_PREFIX);
-        component1 = new AssociatedComponent(REPO_MAVEN_RELEASES, "gr1", "comp1", randomAlphanumeric(5));
+        component1 = new AssociatedComponent(REPO_MAVEN_RELEASES, randomAlphabetic(5), "comp1", "3");
         uploadMavenComponent(component1);
-        component2 = new AssociatedComponent(REPO_MAVEN_RELEASES, "gr2", "comp2", randomAlphanumeric(5));
+        component2 = new AssociatedComponent(REPO_MAVEN_RELEASES, randomAlphabetic(5), "comp2", "1.1.1");
         uploadMavenComponent(component2);
         //TODO: Test non-maven artifacts which does not have group/version
     }
@@ -85,7 +86,7 @@ public class IntegrationTest {
         output.addFormData("maven2.artifactId", component.getName(), MediaType.TEXT_PLAIN_TYPE);
         output.addFormData("maven2.version", component.getVersion(), MediaType.TEXT_PLAIN_TYPE);
         output.addFormData("maven2.asset1.extension", "jar", MediaType.TEXT_PLAIN_TYPE);
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(randomAlphanumeric(100).getBytes());
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(randomAlphabetic(5).getBytes());
         output.addFormData("maven2.asset1", inputStream, MediaType.APPLICATION_OCTET_STREAM_TYPE,
                 "test-maven-artifact.jar");
 
@@ -107,52 +108,66 @@ public class IntegrationTest {
 
     @Test
     public void testCrud() {
-        Map<String, String> attributes = new HashMap<>();
-        attributes.put(CHANGE_ID, randomAlphanumeric(20));
-        attributes.put(STATUS, "failed");
-        attributes.put("Commit-Id", randomAlphanumeric(20));
+        // Add Tags
+        TagDefinition tagDef1 = new TagDefinition(randomAlphanumeric(5), createAttributes(), Arrays.asList(component1));
+        Tag createdTag1 = addTagAndAssert(tagDef1);
 
-        TagDefinition tag = new TagDefinition(randomAlphanumeric(5), attributes, Arrays.asList(component1));
-
-        // Add Tag
-        Response response = addTag(tag);
-        assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-        Tag postResponseTag = response.readEntity(Tag.class);
-        assertFalse(new Date().before(postResponseTag.getFirstCreated()));
-        assertEquals(postResponseTag.getFirstCreated(), postResponseTag.getLastUpdated());
-        assertDefinitionEquals(tag, postResponseTag);
+        TagDefinition tagDef2 = new TagDefinition(randomAlphanumeric(5), createAttributes(),
+                Arrays.asList(component1, component2));
+        addTagAndAssert(tagDef2);
 
         // Test Get by name
-        Tag retrieved = target.path("tags/" + tag.getName()).request().get(Tag.class);
-        assertDefinitionEquals(tag, retrieved);
+        Tag retrieved = target.path("tags/" + tagDef1.getName()).request().get(Tag.class);
+        assertDefinitionEquals(tagDef1, retrieved);
 
         // Test search by attribute and component
         String component1Criterion = String.format("%s:%s:%s = %s", component1.getRepository(), component1.getGroup(),
                 component1.getName(), component1.getVersion());
         List<Tag> result = target.path("tags")
-                .queryParam("attribute", CHANGE_ID + ":" + tag.getAttributes().get(CHANGE_ID))
-                .queryParam("attribute", STATUS + ":" + tag.getAttributes().get(STATUS))
+                .queryParam("attribute", CHANGE_ID + ":" + tagDef1.getAttributes().get(CHANGE_ID))
+                .queryParam("attribute", STATUS + ":" + tagDef1.getAttributes().get(STATUS))
                 .queryParam("associatedComponent", component1Criterion)
                 .request()
                 .get(new GenericType<List<Tag>>() {});
         assertEquals(1, result.size());
-        assertDefinitionEquals(tag, result.get(0));
+        assertDefinitionEquals(tagDef1, result.get(0));
+
+        String component2Criterion = String.format("%s:%s:%s > %s", component2.getRepository(), component2.getGroup(),
+                component2.getName(), "1.1.0");
+        result = target.path("tags")
+                .queryParam("attribute", CHANGE_ID + ":" + tagDef2.getAttributes().get(CHANGE_ID))
+                .queryParam("attribute", STATUS + ":" + tagDef2.getAttributes().get(STATUS))
+                .queryParam("associatedComponent", component1Criterion)
+                .queryParam("associatedComponent", component2Criterion)
+                .request()
+                .get(new GenericType<List<Tag>>() {});
+        assertEquals(1, result.size());
+        assertDefinitionEquals(tagDef2, result.get(0));
 
         // Update tag
-        tag.getAttributes().put(STATUS, "successful");
-        tag.setComponents(Arrays.asList(component1, component2));
-        response = updateTag(tag);
+        tagDef1.getAttributes().put(STATUS, "successful");
+        tagDef1.setComponents(Arrays.asList(component1, component2));
+        Response response = updateTag(tagDef1);
         Tag putResponseTag = response.readEntity(Tag.class);
-        assertEquals(postResponseTag.getFirstCreated(), putResponseTag.getFirstCreated());
+        assertEquals(createdTag1.getFirstCreated(), putResponseTag.getFirstCreated());
         assertFalse(new Date().before(putResponseTag.getLastUpdated()));
         assertFalse(putResponseTag.getLastUpdated().before(putResponseTag.getFirstCreated()));
-        assertDefinitionEquals(tag, putResponseTag);
+        assertDefinitionEquals(tagDef1, putResponseTag);
 
         // Test deleting tag
-        response = target.path("tags/" + tag.getName()).request().delete();
+        response = target.path("tags/" + tagDef1.getName()).request().delete();
         assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-        response = target.path("tags/" + tag.getName()).request().get();
+        response = target.path("tags/" + tagDef1.getName()).request().get();
         assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+    }
+
+
+    private Map<String, String> createAttributes() {
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put(CHANGE_ID, randomAlphanumeric(20));
+        attributes.put(STATUS, "failed");
+        attributes.put("Commit-Id", randomAlphanumeric(20));
+        return attributes;
     }
 
     @Test
@@ -160,14 +175,12 @@ public class IntegrationTest {
         TagDefinition tag = new TagDefinition(randomAlphanumeric(5), singletonMap("attr1", "val1"),
                 Arrays.asList(component1, component2));
         // Add Tag
-        Response response = addTag(tag);
-        assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-        response.close();
+        addTagAndAssert(tag);
 
         // Clone Tag
         String newName = tag.getName() + "-cloned";
         TagCloneRequest request = new TagCloneRequest(tag.getName(), singletonMap("attr2", "val2"));
-        response =
+        Response response =
                 target.path("tags/" + newName).request().post(Entity.entity(request, MediaType.APPLICATION_JSON_TYPE));
         assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
         Tag cloned = response.readEntity(Tag.class);
@@ -211,6 +224,16 @@ public class IntegrationTest {
         response = addTag(withNonExistingComponent);
         assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
         response.close();
+    }
+
+    private Tag addTagAndAssert(TagDefinition tagDefinition) {
+        Response response = addTag(tagDefinition);
+        assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
+        Tag createdTag = response.readEntity(Tag.class);
+        assertFalse(new Date().before(createdTag.getFirstCreated()));
+        assertEquals(createdTag.getFirstCreated(), createdTag.getLastUpdated());
+        assertDefinitionEquals(tagDefinition, createdTag);
+        return createdTag;
     }
 
     private Response addTag(TagDefinition tag) {
